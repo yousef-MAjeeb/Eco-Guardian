f4aef638a16bf1972897b5118f6f47d408c44362
idiot-Dev <3amkhero@gmail.com>
2026-07-03 18:19:03 +0300

    update dependencies

--- Eco-GuardianApp/app/build.gradle.kts
+++ Eco-GuardianApp/app/build.gradle.kts
@@ -51,6 +51,14 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+
+    //material icons
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
+
+    //Navigation-Compose
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
